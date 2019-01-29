e250321065dec1f12c32621bcad535fdc3ac9e25
Dariya <dariyaleleka@gmail.com>
2019-01-30 00:14:07 +0200

    Add Styles to HeroList
    Create New Component - HeroList Element, add styles

--- src/components/HeroListElement/index.js
+++ src/components/HeroListElement/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import './style.css'
-
 function  HeroListElement (props) {
 
 		const {id, image, name, status, species, gender, origin, location} = props.hero;
 
 	return (
 		<section>
-			
+			<article className="CharacterCard_Wrapper">
+				<div data="card header" className="CharacterCard__ImgWrapper">
+					<div className="card-image"><img src="https://rickandmortyapi.com/api/character/avatar/69.jpeg" alt="Commander Rick"/>
+					</div>
+					<div className="CharacterCard__Title">
+						<h2 className="CharacterCard__Name">Commander Rick</h2>
+						<p className="CharacterCard__Description">id: 69 - created a year ago</p>
+					</div>
+				</div>
+				<div data="card info" className="CharacterCard__InfoWrapper">
+					<div className="CharacterCard__TextWrapper">
+						<span>STATUS</span><p>Dead</p></div>
+					<div className="CharacterCard__TextWrapper">
+						<span>SPECIES</span><p>Human</p></div>
+					<div className="CharacterCard__TextWrapper">
+						<span>GENDER</span><p>Male</p></div>
+					<div className="CharacterCard__TextWrapper">
+						<span>ORIGIN</span><p>unknown</p></div>
+					<div className="CharacterCard__TextWrapper">
+						<span>LAST LOCATION</span><p>Citadel of Ricks</p></div>
+				</div>
+
+			</article>
 		</section>
 	)
 };
--- src/components/HerosList/index.js
+++ src/components/HerosList/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
 import HeroListElement from '../HeroListElement';
+import './style.css'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class HeroList extends Component  {
@@ -25,21 +26,9 @@ class HeroList extends Component  {
 	render() {
 
 	return (
-		<section className="Home__show">
-				<div className="Home__showInner">
-					<article className="CharacterCard__Wrapper-sc-19ezuaf-0 fbjIjh">
-
-					<div data="card header" class="CharacterCard__ImgWrapper-sc-19ezuaf-1 bMUkNs">
-					<div class="card-image"><img src="https://rickandmortyapi.com/api/character/avatar/69.jpeg" alt="Commander Rick"/>
-					</div>
-					<div class="CharacterCard__Title-sc-19ezuaf-3 cJYIzS">
-					<h2 class="CharacterCard__Name-sc-19ezuaf-4 gsRlJi">Commander Rick</h2>
-					<p class="CharacterCard__Description-sc-19ezuaf-5 jFxto">id: 69 - created a year ago</p>
-					</div>
-					</div>
-					<div data="card info" class="CharacterCard__InfoWrapper-sc-19ezuaf-2 duzZxp"><div class="CharacterCard__TextWrapper-sc-19ezuaf-6 ABEdB"><span>STATUS</span><p>Dead</p></div><div class="CharacterCard__TextWrapper-sc-19ezuaf-6 ABEdB"><span>SPECIES</span><p>Human</p></div><div class="CharacterCard__TextWrapper-sc-19ezuaf-6 ABEdB"><span>GENDER</span><p>Male</p></div><div class="CharacterCard__TextWrapper-sc-19ezuaf-6 ABEdB"><span>ORIGIN</span><p>unknown</p></div><div class="CharacterCard__TextWrapper-sc-19ezuaf-6 loCIhT"><span>LAST LOCATION</span><p>Citadel of Ricks</p></div></div>
-
-					</article>
+		<section className="Home__showCaseWrapper">
+				<div className="Home__showCaseInner">
+					<HeroListElement/>
 				</div>
 		</section>
 	 )
